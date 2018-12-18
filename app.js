2084142d5904f1e0ac2d4f66e507d425eeffa1ae
fuyunfeng <ex-fuyunfeng@xianglin.cn>
2018-12-18 16:39:38 +0800

    注释

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ App({
 		// logs.unshift(Date.now())
 		// wx.setStorageSync('logs', logs)
 
+		// ajax错误统一弹窗
 		wechat.errDialog = msg => {
 			wx.showToast({
 				title: msg,  //标题
-				icon: 'success ',  //图标，支持"success"、"loading"
+				icon: 'none ',  //图标，支持"success"、"loading"
 				image: '/static/images/err1.png',  //自定义图标的本地路径，image 的优先级高于 icon
 				duration: 2000, //提示的延迟时间，单位毫秒，默认：1500
 				mask: false,  //是否显示透明蒙层，防止触摸穿透，默认：false
